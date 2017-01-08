32c8925987b699bef06537761138e7cf9cf7490d
Wolf2000 <wolf2000@aon.at>
2017-01-08 09:08:33 +0100

    Revert "expand sd Del."
    This reverts commit cb594698c9a616ab2e15915cd33cc565d85be0fd.

--- wolf2000-config.sh
+++ wolf2000-config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Part of Wolf2000-Tools https://github.com/Wolf2000Pi/wolf2000-tools
-# Version 3.2
+# Version 3.1
 # by Wolf2000
 
 INTERACTIVE=True
@@ -31,6 +31,88 @@ Ich hoffe ihr seid zufrieden?
 Für Schäden übernehme ich Keine Haftung!
 @Wolf2000.\
 " 20 70 1
+}
+
+do_expand_rootfs() {
+  if ! [ -h /dev/root ]; then
+    whiptail --msgbox "/dev/root does not exist or is not a symlink. Don't know how to expand" 20 60 2
+    return 0
+  fi
+
+  ROOT_PART=$(readlink /dev/root)
+  PART_NUM=${ROOT_PART#mmcblk0p}
+  if [ "$PART_NUM" = "$ROOT_PART" ]; then
+    whiptail --msgbox "/dev/root is not an SD card. Don't know how to expand" 20 60 2
+    return 0
+  fi
+
+  # NOTE: the NOOBS partition layout confuses parted. For now, let's only 
+  # agree to work with a sufficiently simple partition layout
+  if [ "$PART_NUM" -ne 2 ]; then
+    whiptail --msgbox "Your partition layout is not currently supported by this tool. You are probably using NOOBS, in which case your root filesystem is already expanded anyway." 20 60 2
+    return 0
+  fi
+
+  LAST_PART_NUM=$(parted /dev/mmcblk0 -ms unit s p | tail -n 1 | cut -f 1 -d:)
+
+  if [ "$LAST_PART_NUM" != "$PART_NUM" ]; then
+    whiptail --msgbox "/dev/root is not the last partition. Don't know how to expand" 20 60 2
+    return 0
+  fi
+
+  # Get the starting offset of the root partition
+  PART_START=$(parted /dev/mmcblk0 -ms unit s p | grep "^${PART_NUM}" | cut -f 2 -d:)
+  [ "$PART_START" ] || return 1
+  # Return value will likely be error for fdisk as it fails to reload the
+  # partition table because the root fs is mounted
+  fdisk /dev/mmcblk0 <<EOF
+p
+d
+$PART_NUM
+n
+p
+$PART_NUM
+$PART_START
+
+p
+w
+EOF
+  ASK_TO_REBOOT=1
+
+  # now set up an init.d script
+cat <<\EOF > /etc/init.d/resize2fs_once &&
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          resize2fs_once
+# Required-Start:
+# Required-Stop:
+# Default-Start: 2 3 4 5 S
+# Default-Stop:
+# Short-Description: Resize the root filesystem to fill partition
+# Description:
+### END INIT INFO
+
+. /lib/lsb/init-functions
+
+case "$1" in
+  start)
+    log_daemon_msg "Starting resize2fs_once" &&
+    resize2fs /dev/root &&
+    rm /etc/init.d/resize2fs_once &&
+    update-rc.d resize2fs_once remove &&
+    log_end_msg $?
+    ;;
+  *)
+    echo "Usage: $0 start" >&2
+    exit 3
+    ;;
+esac
+EOF
+  chmod +x /etc/init.d/resize2fs_once &&
+  update-rc.d resize2fs_once defaults &&
+  if [ "$INTERACTIVE" = True ]; then
+    whiptail --msgbox "Root partition has been resized.\nThe filesystem will be enlarged upon the next reboot" 20 60 2
+  fi
 }
 
 set_config_var() {
@@ -70,6 +152,11 @@ end
 EOF
 }
 
+# $1 is 0 to disable overscan, 1 to disable it
+
+
+
+
 do_change_pass() {
   whiptail --msgbox "You will now be asked to enter a new password for the pi user" 20 60 1
   passwd root &&
@@ -108,6 +195,39 @@ No other symbols, punctuation characters, or blank spaces are permitted.\
   fi
 }
 
+do_memory_split() { # Memory Split
+  if [ -e /boot/start_cd.elf ]; then
+    # New-style memory split setting
+    if ! mountpoint -q /boot; then
+      return 1
+    fi
+    ## get current memory split from /boot/config.txt
+    CUR_GPU_MEM=$(get_config_var gpu_mem $CONFIG)
+    [ -z "$CUR_GPU_MEM" ] && CUR_GPU_MEM=64
+    ## ask users what gpu_mem they want
+    NEW_GPU_MEM=$(whiptail --inputbox "How much memory should the GPU have?  e.g. 16/32/64/128/256" \
+      20 70 -- "$CUR_GPU_MEM" 3>&1 1>&2 2>&3)
+    if [ $? -eq 0 ]; then
+      set_config_var gpu_mem "$NEW_GPU_MEM" $CONFIG
+      ASK_TO_REBOOT=1
+    fi
+  else # Old firmware so do start.elf renaming
+    get_current_memory_split
+    MEMSPLIT=$(whiptail --menu "Set memory split.\n$MEMSPLIT_DESCRIPTION" 20 60 10 \
+      "240" "240MiB for ARM, 16MiB for VideoCore" \
+      "224" "224MiB for ARM, 32MiB for VideoCore" \
+      "192" "192MiB for ARM, 64MiB for VideoCore" \
+      "128" "128MiB for ARM, 128MiB for VideoCore" \
+      3>&1 1>&2 2>&3)
+    if [ $? -eq 0 ]; then
+      set_memory_split ${MEMSPLIT}
+      ASK_TO_REBOOT=1
+    fi
+  fi
+}
+
+
+
 do_ssh() {
   if [ -e /var/log/regen_ssh_keys.log ] && ! grep -q "^finished" /var/log/regen_ssh_keys.log; then
     whiptail --msgbox "Initial ssh key generation still running. Please wait and try again." 20 60 2
@@ -125,6 +245,51 @@ do_ssh() {
     whiptail --msgbox "SSH server disabled" 20 60 1
   else
     return $RET
+  fi
+}
+
+do_devicetree() {
+  CURRENT_SETTING="enabled" # assume not disabled
+  DEFAULT=
+  if [ -e $CONFIG ] && grep -q "^device_tree=$" $CONFIG; then
+    CURRENT_SETTING="disabled"
+    DEFAULT=--defaultno
+  fi
+
+  whiptail --yesno "Would you like the kernel to use Device Tree?" $DEFAULT 20 60 2
+  RET=$?
+  if [ $RET -eq 0 ]; then
+    sed $CONFIG -i -e "s/^\(device_tree=\)$/#\1/"
+    sed $CONFIG -i -e "s/^#\(device_tree=.\)/\1/"
+    SETTING=enabled
+  elif [ $RET -eq 1 ]; then
+    sed $CONFIG -i -e "s/^#\(device_tree=\)$/\1/"
+    sed $CONFIG -i -e "s/^\(device_tree=.\)/#\1/"
+    if ! grep -q "^device_tree=$" $CONFIG; then
+      printf "device_tree=\n" >> $CONFIG
+    fi
+    SETTING=disabled
+  else
+    return 0
+  fi
+  TENSE=is
+  REBOOT=
+  if [ $SETTING != $CURRENT_SETTING ]; then
+    TENSE="will be"
+    REBOOT=" after a reboot"
+    ASK_TO_REBOOT=1
+  fi
+  whiptail --msgbox "Device Tree $TENSE $SETTING$REBOOT" 20 60 1
+}
+
+
+disable_raspi_config_at_boot() {
+  if [ -e /etc/profile.d/raspi-config.sh ]; then
+    rm -f /etc/profile.d/raspi-config.sh
+    sed -i /etc/inittab \
+      -e "s/^#\(.*\)#\s*RPICFG_TO_ENABLE\s*/\1/" \
+      -e "/#\s*RPICFG_TO_DISABLE/d"
+    telinit q
   fi
 }
 
@@ -155,6 +320,139 @@ do_finish() {
 get_json_string_val() {
   sed -n -e "s/^[[:space:]]*\"$2\"[[:space:]]*:[[:space:]]*\"\(.*\)\"[[:space:]]*,$/\1/p" $1
 }
+
+do_apply_os_config() {
+  [ -e /boot/os_config.json ] || return 0
+  NOOBSFLAVOUR=$(get_json_string_val /boot/os_config.json flavour)
+  NOOBSLANGUAGE=$(get_json_string_val /boot/os_config.json language)
+  NOOBSKEYBOARD=$(get_json_string_val /boot/os_config.json keyboard)
+
+  if [ -n "$NOOBSFLAVOUR" ]; then
+    printf "Setting flavour to %s based on os_config.json from NOOBS. May take a while\n" "$NOOBSFLAVOUR"
+
+    if printf "%s" "$NOOBSFLAVOUR" | grep -q "Scratch"; then
+      disable_raspi_config_at_boot
+      enable_boot_to_scratch
+    else
+      printf "Unrecognised flavour. Ignoring\n"
+    fi
+  fi
+
+  # TODO: currently ignores en_gb settings as we assume we are running in a 
+  # first boot context, where UK English settings are default
+  case "$NOOBSLANGUAGE" in
+    "en")
+      if [ "$NOOBSKEYBOARD" = "gb" ]; then
+        DEBLANGUAGE="" # UK english is the default, so ignore
+      else
+        DEBLANGUAGE="en_US.UTF-8"
+      fi
+      ;;
+    "de")
+      DEBLANGUAGE="de_DE.UTF-8"
+      ;;
+    "fi")
+      DEBLANGUAGE="fi_FI.UTF-8"
+      ;;
+    "fr")
+      DEBLANGUAGE="fr_FR.UTF-8"
+      ;;
+    "hu")
+      DEBLANGUAGE="hu_HU.UTF-8"
+      ;;
+    "ja")
+      DEBLANGUAGE="ja_JP.UTF-8"
+      ;;
+    "nl")
+      DEBLANGUAGE="nl_NL.UTF-8"
+      ;;
+    "pt")
+      DEBLANGUAGE="pt_PT.UTF-8"
+      ;;
+    "ru")
+      DEBLANGUAGE="ru_RU.UTF-8"
+      ;;
+    "zh_CN")
+      DEBLANGUAGE="zh_CN.UTF-8"
+      ;;
+    *)
+      printf "Language '%s' not handled currently. Run sudo raspi-config to set up" "$NOOBSLANGUAGE"
+      ;;
+  esac
+
+  if [ -n "$DEBLANGUAGE" ]; then
+    printf "Setting language to %s based on os_config.json from NOOBS. May take a while\n" "$DEBLANGUAGE"
+    cat << EOF | debconf-set-selections
+locales   locales/locales_to_be_generated multiselect     $DEBLANGUAGE UTF-8
+EOF
+    rm /etc/locale.gen
+    dpkg-reconfigure -f noninteractive locales
+    update-locale LANG="$DEBLANGUAGE"
+    cat << EOF | debconf-set-selections
+locales   locales/default_environment_locale select       $DEBLANGUAGE
+EOF
+  fi
+
+  if [ -n "$NOOBSKEYBOARD" -a "$NOOBSKEYBOARD" != "gb" ]; then
+    printf "Setting keyboard layout to %s based on os_config.json from NOOBS. May take a while\n" "$NOOBSKEYBOARD"
+    sed -i /etc/default/keyboard -e "s/^XKBLAYOUT.*/XKBLAYOUT=\"$NOOBSKEYBOARD\"/"
+    dpkg-reconfigure -f noninteractive keyboard-configuration
+    invoke-rc.d keyboard-setup start
+  fi
+  return 0
+}
+
+#
+# Command line options for non-interactive use
+#
+for i in $*
+do
+  case $i in
+  --memory-split)
+    OPT_MEMORY_SPLIT=GET
+    printf "Not currently supported\n"
+    exit 1
+    ;;
+  --memory-split=*)
+    OPT_MEMORY_SPLIT=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+    printf "Not currently supported\n"
+    exit 1
+    ;;
+  --expand-rootfs)
+    INTERACTIVE=False
+    do_expand_rootfs
+    printf "Please reboot\n"
+    exit 0
+    ;;
+  --apply-os-config)
+    INTERACTIVE=False
+    do_apply_os_config
+    exit $?
+    ;;
+  *)
+    # unknown option
+    ;;
+  esac
+done
+
+#if [ "GET" = "${OPT_MEMORY_SPLIT:-}" ]; then
+#  set -u # Fail on unset variables
+#  get_current_memory_split
+#  echo $CURRENT_MEMSPLIT
+#  exit 0
+#fi
+
+# Everything else needs to be run as root
+if [ $(id -u) -ne 0 ]; then
+  printf "Script must be run as root. Try 'sudo raspi-config'\n"
+  exit 1
+fi
+
+if [ -n "${OPT_MEMORY_SPLIT:-}" ]; then
+  set -e # Fail when a command errors
+  set_memory_split "${OPT_MEMORY_SPLIT}"
+  exit 0
+fi
 
 do_internationalisation_menu() {
   FUN=$(whiptail --title "Banana Pi Software Configuration Tool (Wolf2000-config)" --menu "Internationalisation Options" $WT_HEIGHT $WT_WIDTH $WT_MENU_HEIGHT --cancel-button Back --ok-button Select \
@@ -237,28 +535,30 @@ do_update_wolf2000() {
 calc_wt_size
 while true; do
   FUN=$(whiptail --title "Banana Pi Software Configuration Tool (Wolf2000-config)" --menu "Setup Options" $WT_HEIGHT $WT_WIDTH $WT_MENU_HEIGHT --cancel-button Finish --ok-button Select \
-    "1 Change User Password" "Root Password ändern" \
-    "2 Internationalisation Options" "Sprache-Zeit-Tastatur " \
-    "3 Advanced Options" "Configure advanced settings" \
-	"4 Update System" "Update und upgrade" \
-	"5 Openmediavault Version 2" "Installation Unter Debian Wheezy" \
-	"6 Openmediavault Version 3" "Installation Unter Debian Jessie" \
-	"7 About wolf2000-config" "Bitte Lesen" \
-	"8 Update" "Wolf2000-Tools Updaten" \
+    "1 Expand Filesystem" "Stellt sicher,dass die ganze SD-Kartenspeicher für das OS verfügbar ist" \
+    "2 Change User Password" "Root Password ändern" \
+    "3 Internationalisation Options" "Sprache-Zeit-Tastatur " \
+    "4 Advanced Options" "Configure advanced settings" \
+	"5 Update System" "Update und upgrade" \
+	"6 Openmediavault Version 2" "Installation Unter Debian Wheezy" \
+	"7 Openmediavault Version 3" "Installation Unter Debian Jessie" \
+	"8 About wolf2000-config" "Bitte Lesen" \
+	"9 Update" "Wolf2000-Tools Updaten" \
     3>&1 1>&2 2>&3)
   RET=$?
   if [ $RET -eq 1 ]; then
     do_finish
   elif [ $RET -eq 0 ]; then
     case "$FUN" in
-      1\ *) do_change_pass ;;
-      2\ *) do_internationalisation_menu ;;
-      3\ *) do_advanced_menu ;;
-      4\ *) do_update ;;
-	  5\ *) do_omv2 ;;
-	  6\ *) do_omv3 ;;
-	  7\ *) do_about ;;
-	  8\ *) do_update_wolf2000 ;;
+      1\ *) do_expand_rootfs ;;
+      2\ *) do_change_pass ;;
+      3\ *) do_internationalisation_menu ;;
+      4\ *) do_advanced_menu ;;
+      5\ *) do_update ;;
+	  6\ *) do_omv2 ;;
+	  7\ *) do_omv3 ;;
+	  8\ *) do_about ;;
+	  9\ *) do_update_wolf2000 ;;
       *) whiptail --msgbox "Programmer error: unrecognized option" 20 60 1 ;;
     esac || whiptail --msgbox "There was an error running option $FUN" 20 60 1
   else
